85fd9bb2ae92b196f4ca9cc70dd03ceff5250a4d
supportQS <90088213+supportQS@users.noreply.github.com>
2024-02-10 22:38:01 +0530

    Files added

---
+++ FirstProject.java
@@ -0,0 +1,7 @@
+// Your First Program
+
+class FirstProject{
+    public static void main(String[] args) {
+        System.out.println("Congrats Mahesh for your first java program in jenkins"); 
+    }
+}
