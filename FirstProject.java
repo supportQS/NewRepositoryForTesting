1a904ecde7c8af7a25591c6e8226df676b2f2be2
supportQS <90088213+supportQS@users.noreply.github.com>
2024-02-11 16:22:05 +0530

    Modified for testing

--- FirstProject.java
+++ FirstProject.java
@@ -3,7 +3,11 @@
 class FirstProject{
     public static void main(String[] args) {
         System.out.println("Congrats Mahesh for your first java program in jenkins"); 
-        System.out.println("Congrats Mahesh for your second java program in jenkins"); 
-        
+	System.out.println("Congrats edited via linux commands for your second
+			java program from Git Bash mode. modified for auto	trigger"); 
+  
+
+
+
     }
 }
