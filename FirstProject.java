b59e32f1650c48dbd8cf6aafc9a6ae1f76fb2e22
supportQS <90088213+supportQS@users.noreply.github.com>
2024-02-11 16:45:04 +0530

    updated'

--- FirstProject.java
+++ FirstProject.java
@@ -2,7 +2,7 @@
 
 class FirstProject{
     public static void main(String[] args) {
-        System.out.println("Congrats Mahesh for your first java program in jenkins"); 
+        System.out.println("Edited Mahesh for your first java program in jenkins"); 
 	
   
 
