3e22624c0d2d234f56fba123ceac38737a370215
supportQS <90088213+supportQS@users.noreply.github.com>
2024-02-11 16:42:48 +0530

    deleted

--- FirstProject.java
+++ FirstProject.java
@@ -3,8 +3,7 @@
 class FirstProject{
     public static void main(String[] args) {
         System.out.println("Congrats Mahesh for your first java program in jenkins"); 
-	System.out.println("Congrats edited via linux commands for your second
-			java program from Git Bash mode. modified for auto	trigger"); 
+	
   
 
 
